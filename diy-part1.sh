3139f865aef5bce5072b29b4900716a00bc85cb5
minyangout <minyang3194@outlook.com>
2024-08-14 20:43:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,5 +22,5 @@
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
-!/bin/bash
+/bin/bash
 
